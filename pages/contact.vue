9c90e0debcabcb8bd75e18dad3eeabfba76ccb77
AMQOR Merouane <marouane.amqor6@gmail.com>
2023-10-23 12:26:03 +0100

    enable contact sections

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,23 +2,23 @@
   <div class="bg-white py-16 px-4 sm:px-6 lg:px-8">
     <!-- Contact Sections -->
     <div class="max-w-7xl mx-auto md:grid md:grid-cols-2 md:gap-8">
-      <!-- Sales Support
+      <!-- Sales Support -->
       <SectionsContactSection 
         :title="$t('contactPage.salesSupport.title')" 
         :description="$t('contactPage.salesSupport.description')" 
         :phone="$t('contactPage.salesSupport.phone')" 
         :hours="$t('contactPage.salesSupport.hours')" 
         :email="$t('contactPage.salesSupport.email')"
-      /> -->
+      />
       
-      <!-- Technical Support
+      <!-- Technical Support -->
       <SectionsContactSection 
         :title="$t('contactPage.technicalSupport.title')" 
         :description="$t('contactPage.technicalSupport.description')" 
         :phone="$t('contactPage.technicalSupport.phone')" 
         :hours="$t('contactPage.technicalSupport.hours')" 
         :email="$t('contactPage.technicalSupport.email')"
-      /> -->
+      /> 
     </div>
 
     <!-- Contact Form -->
